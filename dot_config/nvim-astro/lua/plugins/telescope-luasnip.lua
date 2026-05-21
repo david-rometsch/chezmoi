131f1640fac90b2277038a298a2dc7bae08b6dfe
David Rometsch <david.rometsch@edu.teko.ch>
2026-05-21 22:04:28 +0200

    Remove .config/nvim-astro/README.md Remove .config/nvim-astro/.luarc.json Remove .config/nvim-astro/.neoconf.json Remove .config/nvim-astro/.stylua.toml Remove .config/nvim-astro/test.vue Remove .config/nvim-astro/init.lua Remove .config/nvim-astro/lazy-lock.json Remove .config/nvim-astro/lua/community.lua Remove .config/nvim-astro/lua/lazy_setup.lua Remove .config/nvim-astro/lua/plugins/astrocore.lua Remove .config/nvim-astro/lua/plugins/astrolsp.lua Remove .config/nvim-astro/lua/plugins/astroui.lua Remove .config/nvim-astro/lua/plugins/auto-save.lua Remove .config/nvim-astro/lua/plugins/blink-cmp.lua Remove .config/nvim-astro/lua/plugins/blink-cmp.lua.bak Remove .config/nvim-astro/lua/plugins/catppuccin.lua Remove .config/nvim-astro/lua/plugins/ccc.lua Remove .config/nvim-astro/lua/plugins/dbee.lua.bak Remove .config/nvim-astro/lua/plugins/diffview.lua Remove .config/nvim-astro/lua/plugins/image.lua.bak Remove .config/nvim-astro/lua/plugins/luasnip.lua Remove .config/nvim-astro/lua/plugins/mapping.lua Remove .config/nvim-astro/lua/plugins/mason.lua Remove .config/nvim-astro/lua/plugins/none-ls.lua Remove .config/nvim-astro/lua/plugins/obsidian.lua Remove .config/nvim-astro/lua/plugins/obsidian.lua.bak Remove .config/nvim-astro/lua/plugins/oil.lua Remove .config/nvim-astro/lua/plugins/peek.lua Remove .config/nvim-astro/lua/plugins/plantuml-previewer.lua Remove .config/nvim-astro/lua/plugins/plantuml.lua Remove .config/nvim-astro/lua/plugins/telescope-luasnip.lua Remove .config/nvim-astro/lua/plugins/treesitter.lua Remove .config/nvim-astro/lua/plugins/typst-preview.lua Remove .config/nvim-astro/lua/plugins/typst.lua Remove .config/nvim-astro/lua/plugins/user.lua Remove .config/nvim-astro/lua/plugins/webtools.lua Remove .config/nvim-astro/lua/plugins/yazi.lua Remove .config/nvim-astro/lua/polish.lua Remove .config/nvim-astro/lua/user/snippets/python/mysnip.json Remove .config/nvim-astro/neovim.yml Remove .config/nvim-astro/selene.toml Remove .config/nvim-astro/snippets/all.json Remove .config/nvim-astro/snippets/html.json Remove .config/nvim-astro/snippets/package.json Remove .config/nvim-astro/snippets/python.json Remove .config/nvim-astro/snippets/vue.json Remove .config/nvim-astro/test.py

--- dot_config/nvim-astro/lua/plugins/telescope-luasnip.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/plugins/telescope-luasnip.lua
-return {
-  "benfowler/telescope-luasnip.nvim",
-  dependencies = {
-    "L3MON4D3/LuaSnip",
-    "nvim-telescope/telescope.nvim",
-  },
-  config = function() require("telescope").load_extension "luasnip" end,
-}
